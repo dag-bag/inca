3c0abbca522ea9da406d7243727828fdf286156b
dag-bag <virenderkumar23435@gmail.com>
2023-02-17 13:32:57 +0530

    Update it.

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -2,7 +2,7 @@
 
 import mongoose from "mongoose";
 
-import { GetStaticProps } from "next";
+import { GetServerSideProps, GetStaticProps } from "next";
 import { Toaster } from "react-hot-toast";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import BlurImage from "../../components/utils/BlurImage";
@@ -852,22 +852,22 @@ export default function Example({
 // }
 
 // export default ProductDetails;
-export async function getStaticPaths() {
-  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/allparams`);
-  const posts = await res.json();
+// export async function getStaticPaths() {
+//   const res = await fetch(`${process.env.NEXTAUTH_URL}/api/allparams`);
+//   const posts = await res.json();
 
-  // Get the paths we want to pre-render based on posts
-  const paths = posts.map((slug: string) => ({
-    params: { slug: slug },
-  }));
+//   // Get the paths we want to pre-render based on posts
+//   const paths = posts.map((slug: string) => ({
+//     params: { slug: slug },
+//   }));
 
-  // We'll pre-render only these paths at build time.
-  // { fallback: blocking } will server-render pages
-  // on-demand if the path doesn't exist.
-  return { paths, fallback: "blocking" };
-}
+//   // We'll pre-render only these paths at build time.
+//   // { fallback: blocking } will server-render pages
+//   // on-demand if the path doesn't exist.
+//   return { paths, fallback: "blocking" };
+// }
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   if (!mongoose.connections[0].readyState) {
     await mongoose.connect(`${process.env.MONGODB_URI}`);
   }
