3344651484a449d489500b4ea1a1f5c84fe078c0
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2023-03-04 14:08:29 +0530

    try to fix build error

--- pages/test.tsx
+++ pages/test.tsx
@@ -16,7 +16,7 @@ import { FetchedProductType } from "../types/product";
 
 type Props = {};
 
-function Test({}: Props) {
+function Test({ }: Props) {
   const { data, isLoading } = useQuery(["product"], FetchCartProduct);
   // setTimeout(() => {
   //   setCount((prev) => prev + 1);
@@ -47,14 +47,13 @@ function Example() {
               <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
                 <span>What is your refund policy?</span>
                 <ChevronUpIcon
-                  className={`${
-                    open ? "rotate-180 transform" : ""
-                  } h-5 w-5 text-purple-500`}
+                  className={`${open ? "rotate-180 transform" : ""
+                    } h-5 w-5 text-purple-500`}
                 />
               </Disclosure.Button>
               <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                If you're unhappy with your purchase for any reason, email us
-                within 90 days and we'll refund you in full, no questions asked.
+                If you&apos;re unhappy with your purchase for any reason, email us
+                within 90 days and we&apos;ll refund you in full, no questions asked.
               </Disclosure.Panel>
             </>
           )}
