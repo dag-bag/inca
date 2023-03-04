a5677821df078c79e02fc3a50c866bd45155bf42
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2023-03-04 14:54:09 +0530

    Image Change Animation
    here, I created new component and conditionally show components insted of changing src which automatically pre-fetch image from server.

--- components/Product/ProductImage.tsx
+++ components/Product/ProductImage.tsx
@@ -1,7 +1,5 @@
 /** @format */
-
-/** @format */
-
+import { type Dispatch, type SetStateAction } from "react";
 import Image from "next/image";
 import { useState } from "react";
 import { motion } from "framer-motion";
@@ -18,6 +16,17 @@ type Props = {
   cursor?: boolean;
   onClick?: () => void;
 };
+
+
+type imageComponenetProps = {
+  cursor: boolean;
+  rounded: boolean;
+  isLoading: boolean;
+  onClick: () => void;
+  image: { img: string; alt: string };
+  setLoading: Dispatch<SetStateAction<boolean>>
+
+}
 function cn(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
@@ -33,44 +42,48 @@ export default function ProductImage({
 }: Props) {
   const [isLoading, setLoading] = useState(true);
   const [currentImage, setCurrentImage] = useState(0);
+  const imageProps = { image: image[currentImage], rounded, isLoading, cursor, setLoading, onClick } as imageComponenetProps
 
   return (
     <>
       <motion.div
-        className={`w-full aspect-w-1 aspect-h-1 bg-gray-200  overflow-hidden  ${
-          rounded ? "rounded-xl" : ""
-        }`}
-        key={currentImage}
-        animate={{ opacity: 1 }}
-        initial={{ opacity: 0 }}
-        exit={{ opacity: 0 }}
-        transition={{ delay: 0.1 }}
+        className={`w-full aspect-w-1 aspect-h-1 --bg-gray-200 overflow-hidden  ${rounded ? "rounded-xl" : ""
+          }`}
         onMouseEnter={() => setCurrentImage(1)}
-        onMouseLeave={() => setCurrentImage(0)}
-      >
-        <Image
-          alt={image[currentImage].alt}
-          src={image[currentImage].img}
-          //   fill={true}
-          style={{
-            objectFit: "cover",
-          }}
-          width={300}
-          height={300}
-          quality={100}
-          className={cn(
-            `group-hover:opacity-75 duration-700 ease-in-out object-cover ${
-              rounded ? "rounded-xl" : ""
-            }  ${
-              isLoading
-                ? "grayscale blur-2xl scale-110"
-                : "grayscale-0 blur-0 scale-100"
-            } ${cursor ? "cursor-pointer" : ""}`
-          )}
-          onLoadingComplete={() => setLoading(false)}
-          onClick={onClick}
-        />
+        onMouseLeave={() => setCurrentImage(0)}>
+
+        {currentImage == 0 && <ImageComponent {...imageProps} />}
+        {currentImage == 1 && <ImageComponent {...imageProps} />}
+
       </motion.div>
     </>
   );
+}
+
+const ImageComponent: React.FC<imageComponenetProps> = ({ image, rounded, isLoading, cursor, setLoading, onClick }) => {
+  const MotionImage = motion(Image)
+  return (
+    <MotionImage
+      alt={image.alt}
+      src={image.img}
+      animate={{ opacity: 1 }}
+      initial={{ opacity: .8 }}
+      transition={{ duration: .1 }}
+      style={{
+        objectFit: "cover",
+      }}
+      width={300}
+      height={300}
+      quality={100}
+      className={cn(
+        `group-hover:opacity-75 duration-700 ease-in-out object-cover ${rounded ? "rounded-xl" : ""
+        }  ${isLoading
+          ? "grayscale blur-2xl scale-110"
+          : "grayscale-0 blur-0 scale-100"
+        } ${cursor ? "cursor-pointer" : ""}`
+      )}
+      onLoadingComplete={() => setLoading(false)}
+      onClick={onClick}
+    />
+  )
 }
